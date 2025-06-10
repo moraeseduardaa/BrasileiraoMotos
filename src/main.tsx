d59ce9c5f1b69f07b5129196a50322b106363f20
Kevin Pereira <kevinpics@gmail.com>
2025-06-09 22:37:00 -0300

    Atualizações no site

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "/Projetos/Brasileirao/frontend/src/index.css"; // seu Tailwind e estilos globais
+import "./index.css"; // seu Tailwind e estilos globais
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
